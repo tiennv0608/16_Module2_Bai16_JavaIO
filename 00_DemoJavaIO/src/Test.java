a152559239e6f5a33b71c6e42effcdcb6fb869e8
tiennv0608 <tiennv0608@gmail.com>
2021-05-27 16:24:16 +0700

    b1

---
+++ 00_DemoJavaIO/src/Test.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        System.out.println("simple message");
+
+        System.err.println("error message");
+
+        int i=System.in.read();
+
+        System.out.println((char)i);
+
+
+    }
+}
---
+++ 01_B1_TinhtongcacsotrongFileText/src/ReadFileExample.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class ReadFileExample {
+    public void readFileText(String filePath) {
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            int sum = 0;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+            br.close();
+            System.out.println("Tổng = " + sum);
+        } catch (Exception e) {
+            System.err.println("File không tồn tại or nội dung có lỗi!");
+        }
+    }
+}
